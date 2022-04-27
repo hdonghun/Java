6426a26d96d8ec6844b8f02127a02f130d683ee6
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-04-27 23:09:12 +0900

    Rename MyselfJava/Part3_객체지향핵심/day7/Calc.java to MyselfJava/Part3_객체지향핵심/day7/인터페이스/Calc.java

--- MyselfJava/Part3_객체지향핵심/day7/인터페이스/Calc.java
+++ MyselfJava/Part3_객체지향핵심/day7/인터페이스/Calc.java
@@ -1,4 +1,4 @@
-package day7_ÀÎÅÍÆäÀÌ½º;
+package day7_Ã€ÃÃ…ÃÃ†Ã¤Ã€ÃŒÂ½Âº;
 
 public interface Calc {
 	
@@ -9,5 +9,5 @@ public interface Calc {
 	int substract(int num1, int num2);
 	int times(int num1, int num2);
 	int divide(int num1, int num2);
-//±¸ÇöÇÑ´Ù.!
+//Â±Â¸Ã‡Ã¶Ã‡Ã‘Â´Ã™.!
 }
