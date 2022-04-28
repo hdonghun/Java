8f771621123ebce83d9de6bf0523c255ad5da7ee
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-04-28 13:34:11 +0900

    Add files via upload

---
+++ MyselfJava/Part3_객체지향핵심/day8/BookShelf.java
@@ -0,0 +1,20 @@
+package day8_인터페이스구현하기_상속;
+
+public class BookShelf extends Shelf implements Queue{
+
+	@Override
+	public void enQueue(String title) {
+		shelf.add(title);
+	}
+
+	@Override
+	public String deQueue() {
+		return shelf.remove(0);
+	}
+
+	@Override
+	public int getSize() {
+		return getCount();
+	}
+	
+}
