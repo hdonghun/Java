b3c282b11103e948ead11dee9fcf7aea975e8a97
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-04-30 20:58:50 +0900

    Add files via upload

---
+++ MyselfJava/Part5_자바와자료구조/day10_자료구조/MyListNode.java
@@ -0,0 +1,28 @@
+package day10_자료구조;
+
+public class MyListNode {
+	
+	private String data;       // 자료
+	public MyListNode next;    // 다음 노드를 가리키는 링크
+	
+	public MyListNode(){
+		data = null;
+		next = null;
+	}
+	
+	public MyListNode(String data){
+		this.data = data;
+		this.next = null;
+	}
+	
+	public MyListNode(String data, MyListNode link){
+		this.data = data;
+		this.next = link;
+	}
+	
+	public String getData(){
+		return data;
+	}
+	
+	
+}
