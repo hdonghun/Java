fc77c300c38e3de5a791afcb68447fce8aefc4ee
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-05-14 17:35:43 +0900

    Add files via upload

---
+++ 자바스프링입문/Part4/data.sql
@@ -0,0 +1,14 @@
+call next values for hibernate_sequence;
+insert into user('id', 'name', 'email', 'created_at', 'updated_at') values (1,'derek','derek@fastcampus.com',now(),now());
+
+call next values for hibernate_sequence;
+insert into user('id', 'name', 'email', 'created_at', 'updated_at') values (2,'HDH','HDH@fastcampus.com',now(),now());
+
+call next values for hibernate_sequence;
+insert into user('id', 'name', 'email', 'created_at', 'updated_at') values (3,'martin','martin@slowcampus.com',now(),now());
+
+call next values for hibernate_sequence;
+insert into user('id', 'name', 'email', 'created_at', 'updated_at') values (4,'monika','monika@slowcampus.com',now(),now());
+
+call next values for hibernate_sequence;
+insert into user('id', 'name', 'email', 'created_at', 'updated_at') values (5,'martin','martin@anothercampus.com',now(),now());
